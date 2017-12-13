afb9781c078351155937b22696cd71aea23f1a7a
Le Do <lehoangdo90@gmail.com>
2017-12-14 00:42:43 +0700

    first commit

---
+++ Quotes/AppDelegate.swift
@@ -0,0 +1,127 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  
+  var count = 0
+  let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+  let popover = NSPopover()
+  var eventMonitor: EventMonitor?
+  
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    if let button = statusItem.button {
+      button.image = NSImage(named: NSImage.Name(rawValue: "StatusBarButtonImage"))
+      button.action = #selector(AppDelegate.togglePopover(_:))
+    }
+    
+    popover.contentViewController = QuotesViewController.freshController()
+    
+    eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+      if let strongSelf = self, strongSelf.popover.isShown {
+        strongSelf.closePopover(sender: event)
+      }
+    }
+    
+    let timer = Timer.scheduledTimer(timeInterval: 20*60,
+                                     target: self,
+                                     selector: #selector(showNotification),
+                                     userInfo: nil,
+                                     repeats: true)
+    timer.fire()
+    
+  }
+  
+  
+  @objc func showNotification() -> Void {
+    
+    let notification = NSUserNotification()
+    
+    notification.title = "Hey you"
+    notification.informativeText = "Look far away ;)"
+    notification.soundName = NSUserNotificationDefaultSoundName
+    NSUserNotificationCenter.default.deliver(notification)
+    DispatchQueue.main.asyncAfter(deadline: .now()+5) {
+      
+      notification.title = "HiHi!!!"
+      notification.informativeText = "Sitting up ;)"
+      notification.soundName = "Glass.aiff"
+      NSUserNotificationCenter.default.deliver(notification)
+      
+      NSUserNotificationCenter.default.removeAllDeliveredNotifications()
+    }
+  }
+  
+  func userNotificationCenter(_ center: NSUserNotificationCenter,
+                              shouldPresent notification: NSUserNotification) -> Bool {
+    return true
+  }
+  
+  @objc func togglePopover(_ sender: Any?) {
+    if popover.isShown {
+      closePopover(sender: sender)
+    } else {
+      showPopover(sender: sender)
+    }
+  }
+  
+  func showPopover(sender: Any?) {
+    if let button = statusItem.button {
+      popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+      eventMonitor?.start()
+    }
+  }
+  
+  func closePopover(sender: Any?) {
+    popover.performClose(sender)
+    eventMonitor?.stop()
+  }
+  
+  @objc func printQuote(_ sender: Any?) {
+    let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+    let quoteAuthor = "Mark Twain"
+    
+    print("\(quoteText) — \(quoteAuthor)")
+  }
+  
+  func constructMenu() {
+    let menu = NSMenu()
+    
+    menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
+    menu.addItem(NSMenuItem.separator())
+    menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+    
+    statusItem.menu = menu
+  }
+  
+}
+
